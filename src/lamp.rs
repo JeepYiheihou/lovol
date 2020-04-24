c2ba973babc064e11eaa0396a547634eed962c08
haruka28 <57014934+haruka28@users.noreply.github.com>
2020-04-23 19:40:56 -0700

    Basic operations for lamp.

--- src/lamp.rs
+++ src/lamp.rs
@@ -4,19 +4,29 @@ pub struct Lamp {
 }
 
 impl Lamp {
-    pub fn new(is_on: bool, is_dont_disturb: bool)->Lamp {
-        return Lamp{ is_on: is_on, is_dont_disturb: is_dont_disturb };
+    pub fn new(is_on: bool, is_dont_disturb: bool) -> Lamp {
+        return Lamp {
+            is_on: is_on,
+            is_dont_disturb: is_dont_disturb
+        };
     }
 
+    // Turns on the light unless in DND mode. Triggered when receiving remote signal to turn on.
     pub fn turn_on(&mut self) {
         self.is_on = true;
     }
 
+    // Turns off the light. Triggered when receiving remote signal to turn off.
     pub fn turn_off(&mut self) {
         self.is_on = false;
     }
 
-    pub fn turn_switch(&mut self) {
+    // Toggles the light's on/off status and send it to the other lamp.
+    pub fn toggle(&mut self) {
+        if self.is_dont_disturb {
+            return;
+        }
         self.is_on = !self.is_on;
+        // TODO: Call update lamps.
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod lamp;
 
 fn main() {
     let mut _local_lamp : lamp::Lamp = lamp::Lamp::new(false, false);
+    println!("Hello, lovol! before {} ", _local_lamp.is_on);
     _local_lamp.turn_on();
-    println!("Hello, lovol!");
+    println!("Hello, lovol! after {} ", _local_lamp.is_on);
 }
