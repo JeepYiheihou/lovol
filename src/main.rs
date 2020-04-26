dd1f53a26aefc723814fbd5d8a675fd38511718d
JeepYiheihou <646329483@qq.com>
2020-04-26 15:17:10 -0700

    async server

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,14 @@ mod lohse;
 mod server;
 
 use std::io::{Error};
+use std::error::Error;
+use std::net::SocketAddr;
+use std::{env, io};
+use tokio;
+use tokio::net::UdpSocket;
 
-fn main() -> Result<(), Error> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let port = 8196;
     let mut _local_lamp : lamp::Lamp = lamp::Lamp::new(false, true);
 
@@ -22,6 +28,29 @@ fn main() -> Result<(), Error> {
     };
 
     println!("Hello, lovol!");
+    Ok(())
+}
+
+
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+
+    let socket = UdpSocket::bind(&addr).await?;
+    println!("Listening on: {}", socket.local_addr()?);
+
+    let server = Server {
+        socket,
+        buf: vec![0; 1024],
+        to_send: None,
+    };
+
+    // This starts the server task.
+    server.run().await?;
 
     Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -1,29 +1,51 @@
-use std::net::{SocketAddrV4, Ipv4Addr, TcpListener};
-use std::io::{Read, Error};
+use std::error::Error;
+use std::net::SocketAddr;
+use std::{env, io};
+use tokio;
+use tokio::net::UdpSocket;
 
 pub struct Server {
-    loopback: Ipv4Addr,
+    loopback: String,
     port: u16
 }
 
 impl Server {
     pub fn new(port: u16) -> Server {
         return Server {
-            loopback: Ipv4Addr::new(127, 0, 0, 1),
+            loopback: String::from("127.0.0.1"),
             port: port
         };
     }
 
-    pub fn start(&mut self) -> Result<(), Error> {
-        let socket = SocketAddrV4::new(self.loopback, self.port);
-        let listener = TcpListener::bind(socket)?;
-        println!("Listening on {}, access this port to end the program", self.port);
-        loop {
-            let (mut tcp_stream, addr) = listener.accept()?; //block  until requested
-            println!("Connection received! {:?} is sending data.", addr);
-            let mut input = String::new();
-            let _ = tcp_stream.read_to_string(&mut input)?;
-            println!("{:?} says {}", addr, input);
-        }
+    async fn run(& mut self) -> Result<(), Error> {
+        let Server {
+            mut socket,
+            mut buf,
+            mut to_send,
+        } = self;
+
+        let mut listener = TcpListener::bind(&addr).await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            loop {
+                let (mut socket, _) = listener.accept().await?;
+
+                let n = socket
+                    .read(&mut buf)
+                    .await
+                    .expect("failed to read data from socket");
+
+                if n == 0 {
+                    return;
+                }
+
+                socket
+                    .write_all(&buf[0..n])
+                    .await
+                    .expect("failed to write data to socket");
+            }
+        });
     }
 }
